69045f30e974c6e9f90ce3c034c368c576bb654d
William Benton <self@willbenton.com>
2015-01-29 11:08:08 -0600

    added build defs

---
+++ project/Build.scala
@@ -0,0 +1,46 @@
+import sbt._
+import Keys._
+
+object FirkinBuild  extends Build {
+  val VERSION = "0.0.1-SNAPSHOT"
+  
+  lazy val common = project settings(commonSettings : _*)
+  
+  lazy val root = (project in file(".")).aggregate(common)
+  
+  def baseSettings = Defaults.defaultSettings ++ Seq(
+    organization := "com.freevariable",
+    version := VERSION,
+    scalaVersion := SCALA_VERSION,
+    resolvers ++= Seq(
+      "Akka Repo" at "http://repo.akka.io/repository",
+      "spray" at "http://repo.spray.io/"
+    ),
+    scalacOptions ++= Seq("-feature", "-Yrepl-sync", "-target:jvm-1.7", "-Xlint")
+  )
+  
+  def jsonSettings = Seq(
+    libraryDependencies ++= Seq(
+      "org.json4s" %% "json4s-jackson" % JSON4S_VERSION,
+      "org.json4s" %% "json4s-ext" % JSON4S_VERSION,
+      "joda-time" % "joda-time" % "2.5"
+    ) 
+  )
+
+  def colossusSettings = Seq(
+    libraryDependencies ++= Seq(
+      "com.tumblr" %% "colossus" % "0.5.1",
+      "com.typesafe.akka" %% "akka-actor"   % AKKA_VERSION,
+      "com.typesafe.akka" %% "akka-agent"   % AKKA_VERSION,
+      "com.typesafe.akka" %% "akka-testkit" % AKKA_VERSION
+    ) 
+  )
+  
+  def commonSettings = baseSettings ++ colossusSettings ++ jsonSettings
+  
+  val SCALA_VERSION = "2.10.4"
+  
+  val JSON4S_VERSION = "3.2.10"
+  
+  val AKKA_VERSION = "2.3.9"
+}
