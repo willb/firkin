78d1bb2e125af18b9d6aef6d6f2a6f68b76aa021
William Benton <self@willbenton.com>
2015-02-02 11:13:52 -0600

    added bintray integration

--- project/Build.scala
+++ project/Build.scala
@@ -1,16 +1,18 @@
 import sbt._
 import Keys._
 
+import bintray.Plugin._
+
 object FirkinBuild  extends Build {
   val VERSION = "0.1.1-SNAPSHOT"
   
-  lazy val common = project settings(commonSettings : _*)
+  lazy val common = project settings(commonSettings ++ bintrayPublishSettings: _*)
 
   lazy val server = project settings(serverSettings : _*) dependsOn(common, client)
   
-  lazy val client = project settings(clientSettings : _*)
-  
-  lazy val root = (project in file(".")).aggregate(common)
+  lazy val client = project settings(clientSettings ++ bintrayPublishSettings: _*)
+    
+  lazy val root = (project in file(".")).aggregate(common, client)
   
   lazy val serverRun = taskKey[Unit]("Run a Firkin server.")
   
@@ -22,14 +24,14 @@ object FirkinBuild  extends Build {
       "Akka Repo" at "http://repo.akka.io/repository",
       "spray" at "http://repo.spray.io/"
     ),
+    crossScalaVersions := Seq("2.10.4", "2.11.5"),
     scalacOptions ++= Seq("-feature", "-Yrepl-sync", "-target:jvm-1.7", "-Xlint")
   )
   
   def jsonSettings = Seq(
     libraryDependencies ++= Seq(
       "org.json4s" %% "json4s-jackson" % JSON4S_VERSION,
-      "org.json4s" %% "json4s-ext" % JSON4S_VERSION,
-      "joda-time" % "joda-time" % "2.5"
+      "org.json4s" %% "json4s-ext" % JSON4S_VERSION
     ) 
   )
 
@@ -42,9 +44,14 @@ object FirkinBuild  extends Build {
     ) 
   )
   
-  def commonSettings = baseSettings ++ colossusSettings ++ jsonSettings
+  def commonSettings = baseSettings ++ colossusSettings ++ jsonSettings ++ Seq(
+    name := "firkin",
+    crossScalaVersions := Seq("2.10.4", "2.11.5")
+  )
   
-  def clientSettings = jsonSettings ++ Seq(
+  def clientSettings = baseSettings ++ jsonSettings ++ Seq(
+    name := "firkin-client",
+    crossScalaVersions := Seq("2.10.4", "2.11.5"),
     libraryDependencies ++= Seq(
       "net.databinder.dispatch" %% "dispatch-core" % "0.11.1"
     )
